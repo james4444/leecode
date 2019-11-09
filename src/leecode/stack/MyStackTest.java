ae973a372a86c100cfb98eeefc5196d01d5d8af3
603172131@qq.com <wucaiming*0504>
2019-11-09 10:58:34 +0800

    用队列实现栈

---
+++ src/leecode/stack/MyStackTest.java
@@ -0,0 +1,18 @@
+package leecode.stack;
+
+/**
+ * 用队列实现栈
+ */
+public class MyStackTest {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        stack.push(1);
+        stack.push(2);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.top());
+        System.out.println(stack.empty());
+        System.out.println(stack.pop());
+        System.out.println(stack.empty());
+    }
+}
