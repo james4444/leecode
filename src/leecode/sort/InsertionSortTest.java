9c0215acb388fe548d6db28a4c50e8c1837be32a
603172131@qq.com <wucaiming*0504>
2019-11-29 18:14:50 +0800

    插入排序

--- src/leecode/sort/InsertionSortTest.java
+++ src/leecode/sort/InsertionSortTest.java
@@ -7,23 +7,18 @@ public class InsertionSortTest {
      * @return
      */
     public static int[] sort(int[] nums){
-        if(nums == null || nums.length <=1) return nums;
-
-        for(int i=1; i<nums.length; i++){
+        for(int i=1;i<nums.length;i++){
             int j = i-1;
-            int val = nums[i];
-
-            for(; j>=0; j--){
-                if(val < nums[j]){
+            int num = nums[i];
+            for(;j>=0;j--){
+                if(nums[j]>nums[i]){
+                    int temp = nums[j];
                     nums[j+1] = nums[j];
-                }else{
-                    break;
+                    nums[j] = temp;
                 }
             }
-
-            nums[j+1] = val;
+            nums[j+1] = num;
         }
-
         return nums;
     }
 }
