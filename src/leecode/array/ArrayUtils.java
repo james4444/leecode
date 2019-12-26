fb4185bb70d52f1ee0884742e1c3c6f273032a03
603172131@qq.com <wucaiming*0504>
2019-12-26 08:44:49 +0800

    工具类，增加打印数组功能

---
+++ src/leecode/array/ArrayUtils.java
@@ -0,0 +1,9 @@
+package leecode.array;
+
+public class ArrayUtils {
+    public static void printArray(int[] a){
+        for(int num : a){
+            System.out.println(num);
+        }
+    }
+}
